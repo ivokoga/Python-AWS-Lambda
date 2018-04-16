5854bb7a501165d4eac4f33e35f752b5035c6574
Ivo Koga <ivo.koga@gmail.com>
2018-04-15 21:02:39 -0300

    testing...

--- index.py
+++ index.py
@@ -4,8 +4,9 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'output': 'Hello World 2',
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'name': 'Koga'
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
